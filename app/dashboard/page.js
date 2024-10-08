2ac8c18e1b655006efe458c3509ed180e6d90fcc
ZuchiSpeed <zuchi737@gmail.com>
2024-10-08 16:06:40 +0200

    shadcn/themeProvider

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,9 +1,19 @@
+"use client"
+
+import { useTheme } from 'next-themes'
 import React from 'react'
+import { useEffect } from 'react'
 
 function dashboard() {
-  return (
-    <div>dashboard</div>
-  )
+    const { setTheme } = useTheme()
+
+    useEffect(() => {
+        setTheme('system')
+    }, [])
+
+    return (
+        <div>dashboard</div>
+    )
 }
 
 export default dashboard
